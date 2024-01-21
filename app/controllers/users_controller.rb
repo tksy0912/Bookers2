b64639472f09bb7a2aa28054b0f5850977c69507
EC2 Default User <ec2-user@ip-172-31-15-227.ap-northeast-1.compute.internal>
2024-01-21 10:12:59 +0000

    [Update] テストでのエラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  
+
   before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -22,24 +22,24 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def index
     @users = User.all
     @user_info = User.find(current_user.id)
     @book = Book.new
   end
-  
-  
+
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to users_path
+      redirect_to user_path(current_user.id)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
-  
-  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
-  validates :introduction, presence: true, length: { maximum: 50 }
-  
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
