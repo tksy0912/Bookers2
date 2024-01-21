2ca95bc5963a6a9c9aff03a238cb0d36c43c13e9
EC2 Default User <ec2-user@ip-172-31-15-227.ap-northeast-1.compute.internal>
2024-01-21 07:32:29 +0000

    [Update] バリデーションの文字数追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :introduction, presence: true, length: { maximum: 50 }
   
   has_one_attached :profile_image
   
